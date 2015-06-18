a5f2c2be316c82b2283c60d77ae37c52ad33cf8c
AbiSri <AbiSri@bergen.org>
2015-06-18 18:00:15 -0400

    Main menu connected to other games

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GameControl : MonoBehaviour {
 
+	public GameObject BattleSelect;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +12,11 @@ public class GameControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void change(string which) {
+		Debug.Log ("DF:SKFJS:KDLFJS");
+		Application.LoadLevel (which);
+		Debug.Log ("DF:SKFJS:KDLFJS");
 	}
 }
